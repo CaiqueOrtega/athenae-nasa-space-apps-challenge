2a55cae763fda3d3afdbae4488eebb28079e2ae4
JoaoAlavarse <joao.almeida.alavarse@gmail.com>
2024-10-05 20:58:04 -0300

    fix: main.tsx css import

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/globalStyle.css'
 import { RouterProvider } from 'react-router-dom'
+import './assets/styles/globalStyle.css'
 import { Router } from './router'
 
 createRoot(document.getElementById('root')!).render(
